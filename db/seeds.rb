5868b2428fa551a445da024cfdec338e4326c605
Gerald <gmorales>
2019-04-15 14:26:17 -0600

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog}",
+    body: "This is the body number #{blog}"
+  )
+end
+
+puts "10 blog posts created"
+
+9.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 9.1
+  )
+end
+
+puts "9 skills created"
+
+10.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "This is the portfolio item body number #{portfolio_item}",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+puts "10 portfolio items created"
