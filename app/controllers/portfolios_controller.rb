dfba506fdc9e5ac59c18d4174d9562d851ce1ef4
Gerald <gmorales>
2019-04-15 16:11:36 -0600

    Integrated new and create to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,28 @@ class PortfoliosController < ApplicationController
   def index
 	  @portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+  # POST /portfolios.json
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:title,:subtitle,:body,:main_image,:thumb_image)
+    end
 end
