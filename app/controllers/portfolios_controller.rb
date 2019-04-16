57dfa7555311059927807a8762dfe656c654dd45
Gerald <gmorales>
2019-04-16 08:39:50 -0600

    Integrated delete option

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,6 +44,17 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @portfolio.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
