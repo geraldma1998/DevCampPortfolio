f3b3bdb92b0f99c78b375d966cf3e4661d370ba9
Gerald <gmorales>
2019-04-16 14:03:37 -0600

    Implemented model validations for blog, portfolio and skill

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -14,5 +14,7 @@ class Blog < ApplicationRecord
 	extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :body
+
   has_many :comments
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -13,4 +13,5 @@
 #
 
 class Portfolio < ApplicationRecord
+	validates_presence_of :title, :body, :subtitle, :main_image, :thumb_image
 end
