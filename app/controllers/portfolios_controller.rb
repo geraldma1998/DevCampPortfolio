bb554769cb35523f2d605a0a848956a0b4b9dfc6
Gerald <gmorales>
2019-04-16 08:30:55 -0600

    Integrated show option

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
 	  @portfolio_items = Portfolio.all
 	end
@@ -6,6 +8,12 @@ class PortfoliosController < ApplicationController
 	def new
 		@portfolio_item = Portfolio.new
 	end
+
+	def edit
+	end
+
+  def show
+  end
 
   # POST /portfolios.json
   def create
@@ -22,7 +30,26 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  # PATCH/PUT /portfolios/1
+  # PATCH/PUT /portfolios/1.json
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Porfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @portfolio = Portfolio.find(params[:id])
+    end
+
     def portfolio_params
       params.require(:portfolio).permit(:title,:subtitle,:body,:main_image,:thumb_image)
     end
