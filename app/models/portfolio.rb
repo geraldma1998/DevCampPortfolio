296ee69a26969f8b115154b8785b2b8f16412be1
Gerald <gmorales>
2019-04-17 08:58:15 -0600

    Implemented defaults for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -20,4 +20,12 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :search_ruby_on_rails_items, -> {where(subtitle: 'Ruby on rails')} 
+
+	after_initialize :set_defaults
+ 
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400v"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
+
 end
