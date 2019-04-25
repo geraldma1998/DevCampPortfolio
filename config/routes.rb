7f0b521f808d4b3466a5a0a7e51fba4836dc936f
skylar80004 <josedavidvm04@hotmail.com>
2019-04-25 13:08:07 -0600

    Implemented get resources via sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' } 
+  
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular', as: 'angular_items'
-  get 'portfolio/:id', to: 'portfolios#show', as: 'postolio_show'
+  get 'portfolios/:id', to: 'portfolios#show', as: 'postolio_show'
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
