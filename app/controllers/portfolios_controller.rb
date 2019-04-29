100563c5a7c84db5e17ac83f59bfb7773f1335ac
lelthar <geraldm1998@hotmail.com>
2019-04-29 10:03:41 -0600

    Implemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,12 +40,12 @@ class PortfoliosController < ApplicationController
   # PATCH/PUT /portfolios/1.json
   def update
     respond_to do |format|
-      if @portfolio.update(portfolio_params)
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Porfolio was successfully updated.' }
         format.json { render :show, status: :ok, location: @portfolio }
       else
         format.html { render :edit }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +53,7 @@ class PortfoliosController < ApplicationController
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
-    @portfolio.destroy
+    @portfolio_item.destroy
     
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
@@ -64,7 +64,7 @@ class PortfoliosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @portfolio = Portfolio.find(params[:id])
+      @portfolio_item = Portfolio.find(params[:id])
     end
 
     def portfolio_params
