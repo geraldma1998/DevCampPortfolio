2605f14198f6a4eb73260ce71a470dbf652e2232
lelthar <geraldm1998@hotmail.com>
2019-04-30 10:25:58 -0600

    Implemented own gem for generate copyright

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'friendly_id', '~> 5.2.4'
 gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
+
+gem 'devcamp_copyright_tool'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = DevcampCopyrightTool::Renderer.copyright "Gerald Morales", "All rights reserved"
+  end
 end
