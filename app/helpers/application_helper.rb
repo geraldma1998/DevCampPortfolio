7459bf967c9c64b11fae8e24e81e8564230fdde0
lelthar <geraldm1998@hotmail.com>
2019-04-30 08:16:05 -0600

    Added bootstrap to the project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
       link_to 'Logout', destroy_user_session_path, method: :delete
     else    
       (link_to 'Register', new_user_session_path) +
-      "<br>".html_safe +
+      tag(:br) +
       (link_to 'Login', new_user_registration_path)
     end
 	end
